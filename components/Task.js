299f63255d2611d3b78c167b456501446d2c661e
dragosbus <dragosbusuioc94@gmail.com>
2018-07-29 19:39:35 +0200

    init remove task

--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,16 @@
 import React from 'React';
-import { View, Text, Switch, StyleSheet } from 'react-native';
+import { View, Text, Switch, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 
 const Task = props => {
   return (
     <View style={styles.task}>
       <Switch value={props.completed} onValueChange={()=>props.toggleComplete(props.id)} />
       <Text>{props.name}</Text>
+      <ScrollView style={styles.scrollRemoveBtn}>
+          <TouchableOpacity style={styles.removeBtn}>
+              <Text style={styles.removeBtnText}>Remove</Text>
+          </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
@@ -13,6 +18,9 @@ const Task = props => {
 const styles = StyleSheet.create({
   task: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
     width: '80%',
     marginTop: 15,
     marginLeft: 'auto',
@@ -21,6 +29,20 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(2,2,2,0.25)',
     borderRadius: 5,
     padding: 10
+  },
+  taskName: {
+      fontSize: 30,
+      marginLeft: 30
+  },
+  scrollRemoveBtn: {
+      width: 100
+  },
+  removeBtn: {
+      borderWidth: 1
+  },
+  removeBtnText: {
+      backgroundColor: 'red',
+      color: '#fff'
   }
 });
 
